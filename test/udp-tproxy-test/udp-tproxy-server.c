16854e2b7f2ea592ca655200eea8163d264eec91
lif <lif@qpep-clt.myguest.virtualbox.org>
2025-05-30 16:19:23 -0400

    adding udp tproxy test

--- test/udp-tproxy-test/udp-tproxy-server.c
+++ test/udp-tproxy-test/udp-tproxy-server.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
     struct sockaddr_in serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(port);
+    serveraddr.sin_port = htons(9443);
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if (bind(fd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0 ) {
@@ -31,18 +31,21 @@ int main(int argc, char **argv)
     }
 
     int enable = 1;
-#if 0
-    if (setsockopt(fd, IPPROTO_IP, IP_PKTINFO, &enable, sizeof(enable)) < 0) { 
-        perror("setsockopt IP_PKTINFO failed.");
+
+    if (setsockopt(fd, SOL_IP, IP_TRANSPARENT, &enable, sizeof(int)) != 0) {
+        perror("setsockopt(IP_TRANSPARENT) failed.");
+        return -1;
+    }
+
+    if (setsockopt(fd, IPPROTO_IP, IP_RECVOPTS, &enable, sizeof(int)) < 0) { 
+        perror("setsockopt IP_RECVOPTS failed.");
 	return -1;
     }
-#endif 
 
-    if (setsockopt(fd, IPPROTO_IP, IP_RECVORIGDSTADDR, &enable, sizeof(enable)) < 0) { 
-        perror("setsockopt IP_RECVORIGDST failed.");
+    if (setsockopt(fd, SOL_IP, IP_ORIGDSTADDR, &enable, sizeof(int)) < 0) { 
+        perror("setsockopt IP_ORIGDSTADDR failed.");
 	return -1;
     } 
-
 
     struct iovec iov; 
     struct msghdr msg; 
@@ -52,7 +55,7 @@ int main(int argc, char **argv)
     char cmsg_buf[CMSG_SPACE(sizeof(struct in_pktinfo))];
 
     struct sockaddr_in srcaddr;
-    struct sockaddr_in dstaddr; 
+    struct sockaddr_in dstaddr, temp; 
     iov.iov_base = rcv_buf;
     iov.iov_len = sizeof(rcv_buf);
     msg.msg_control = cmsg_buf;
@@ -60,24 +63,31 @@ int main(int argc, char **argv)
     msg.msg_flags = 0;
     msg.msg_iov = &iov;
     msg.msg_iovlen = 1;
-    msg.msg_name = &srcaddr; 
+    msg.msg_name = &temp; 
 
     printf("start listening on UDP port %d\n", port);
+    bzero(&srcaddr, sizeof(srcaddr));
+    bzero(&dstaddr, sizeof(srcaddr));
 
     char buffer[4096];
     while (1) {
-	int len = recvmsg(fd, &msg, 0); 
-        //store the source address first.
-	//memcpy(&srcaddr, msg.msg_name, sizeof(srcaddr));
+	int len = recvmsg(fd, &msg, MSG_OOB); 
+        memcpy(&srcaddr, msg.msg_name, sizeof(srcaddr));
+        int i = 0;
 	for (cmsgtmp = CMSG_FIRSTHDR(&msg); cmsgtmp != NULL; cmsgtmp = CMSG_NXTHDR(&msg, cmsgtmp)) { 
+            i += 1;
+	    printf("iter: %d, cmsg_level: %d, cmsg_type: %d \n", i, cmsgtmp->cmsg_level, cmsgtmp->cmsg_type);
 	    if (cmsgtmp->cmsg_level == SOL_IP && cmsgtmp->cmsg_type == IP_ORIGDSTADDR) { 
+		printf("IP_ORIGDSTADDR is found.\n");
                 struct sockaddr_in *p = (struct sockaddr_in *)CMSG_DATA(cmsgtmp); 
 		memcpy(&dstaddr, p, sizeof(dstaddr));
-		printf("original dst ip: %s:%d  \n", inet_ntoa(p->sin_addr), ntohs(p->sin_port));
-		break;
 	    }
 	}
+
+        printf("original src ip: %s:%d, -> ", inet_ntoa(srcaddr.sin_addr), ntohs(srcaddr.sin_port));
+        printf("  dst ip: %s:%d  \n", inet_ntoa(dstaddr.sin_addr), ntohs(dstaddr.sin_port));
      
+        //printf("inet_ntoa() must be funny. original src ip: %s:%d, -> %s:%d\n", inet_ntoa(srcaddr.sin_addr), ntohs(srcaddr.sin_port), inet_ntoa(dstaddr.sin_addr), ntohs(dstaddr.sin_port));
     }
 
     close(fd);
