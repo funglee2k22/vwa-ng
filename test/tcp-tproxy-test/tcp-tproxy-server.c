3910a4483170ea78af9b268696d034156f2b7aee
lif <lif@qpep-clt.myguest.virtualbox.org>
2025-06-03 19:46:39 -0400

    adding test case for tcp tproxy

--- test/tcp-tproxy-test/tcp-tproxy-server.c
+++ test/tcp-tproxy-test/tcp-tproxy-server.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <linux/netfilter_ipv4.h>
 #include <ev.h>
 
 #define PORT_NO 8443
@@ -16,11 +19,10 @@ int main()
    struct ev_loop *loop = ev_default_loop(0);
    int sd;
    struct sockaddr_in addr;
-   int addr_len = sizeof(addr);
    struct ev_io w_accept;
 
    // Create server socket
-   if ((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+   if ((sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
        perror("socket error");
        return -1;
    }
@@ -35,11 +37,19 @@ int main()
        perror("bind error");
    }
 
+   int enable = 1;
+   if (setsockopt(sd, SOL_IP, IP_TRANSPARENT, &enable, sizeof(int)) != 0) {
+       perror("setsockopt(IP_TRANSPARENT) failed.");
+       return -1;
+   }
+
    // Start listing on the socket
-   if (listen(sd, 2) < 0) {
+   if (listen(sd, 128) < 0) {
        perror("listen error");
        return -1;
    }
+
+   printf("starting listening on port %d.\n", PORT_NO);
 
    // Initialize and start a watcher to accepts client requests
    ev_io_init(&w_accept, accept_cb, sd, EV_READ);
@@ -48,7 +58,7 @@ int main()
    // Start infinite loop
    while (1) {
        ev_loop(loop, 0);
-   } 
+   }
    return 0;
 }
 
@@ -72,6 +82,25 @@ void accept_cb(struct ev_loop *loop, struct ev_io *watcher, int revents)
         perror("accept error");
         return;
     }
+
+    printf("tcp from %s: %d -> ",  inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    struct sockaddr_in server_addr;
+    socklen_t sa_len = sizeof(server_addr);
+    if (getsockopt(client_sd, SOL_IP, SO_ORIGINAL_DST, &server_addr, &sa_len) != 0) {
+        perror("getsockopt(SO_ORIGINAL_DST) failed");
+        exit(-1);
+    }
+    printf(" %s: %d w/ getsockopt(SO_ORIGINAL_DST) \n",  inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+
+    bzero(&server_addr, sizeof(server_addr));
+    if (getsockname(client_sd, (struct sockaddr *) &server_addr, &sa_len) != 0) {
+        perror("getsockname() failed");
+        exit(-1);
+    }
+
+    printf("tcp from %s: %d -> ",  inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    printf(" %s: %d w/ getsockname() \n",  inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
 
     total_clients ++; // Increment total_clients count
     printf("Successfully connected with client.\n");
